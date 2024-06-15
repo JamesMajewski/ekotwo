72062976243a684bd0336a5e8db0a06bc4f18dcf
Jakub Majewski <jakubmajewski@MacBook-Air-Jakub.local>
2024-06-16 01:39:22 +0200

    ChromeDriver nw

--- install_chromedriver.sh
+++ install_chromedriver.sh
@@ -9,13 +9,8 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt-get install -y ./google-chrome-stable_current_amd64.deb
 rm google-chrome-stable_current_amd64.deb
 
-# Get Chrome version
-CHROME_VERSION=$(google-chrome --version | grep -oP '\d+\.\d+\.\d+')
-
-# Find the corresponding ChromeDriver version
-CHROMEDRIVER_VERSION=$(curl -sS https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION)
-
-# Install ChromeDriver
-wget -O /tmp/chromedriver.zip https://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip
+# Install ChromeDriver for a specific Chrome version
+CHROME_DRIVER_VERSION="114.0.5735.90"
+wget -O /tmp/chromedriver.zip https://chromedriver.storage.googleapis.com/${CHROME_DRIVER_VERSION}/chromedriver_linux64.zip
 unzip /tmp/chromedriver.zip -d /usr/local/bin/
 rm /tmp/chromedriver.zip
